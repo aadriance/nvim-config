4ce2327ae544eee1fddf2bef104509d5e22c8dd5
aadriance <primaryartemis@outlook.com>
2023-05-29 19:53:01 -0700

    fixup telescope mapping

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,20 +13,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 --Add leader shortcuts
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files)
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
-vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
-vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>so', function()
-  require('telescope.builtin').tags { only_current_buffer = true }
-end)
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
-
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+--vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,15 +64,12 @@ return {
       version = false, -- telescope did only one release, so use HEAD for now
       keys = {
         { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-        --{ "<leader>/", require('telescope.builtin').live_grep, desc = "Grep (root dir)" },
+        { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (root dir)" },
         { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-        --{ "<leader><space>", require('telescope.builtin').files, desc = "Find Files (root dir)" },
+        { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (root dir)" },
         -- find
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-        --{ "<leader>ff", require('telescope.builtin').files, desc = "Find Files (root dir)" },
-        --{ "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
         { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-        --{ "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
         -- git
         { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
         { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
@@ -83,8 +80,6 @@ return {
         { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
         { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
         { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
-        --{ "<leader>sg", Util.telescope("live_grep"), desc = "Grep (root dir)" },
-        --{ "<leader>sG", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
         { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
         { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
         { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
@@ -92,45 +87,7 @@ return {
         { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
         { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
         { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-        --{ "<leader>sw", Util.telescope("grep_string"), desc = "Word (root dir)" },
-        --{ "<leader>sW", Util.telescope("grep_string", { cwd = false }), desc = "Word (cwd)" },
-        --{ "<leader>uC", Util.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
-        --{
-          --"<leader>ss",
-          --Util.telescope("lsp_document_symbols", {
-            --symbols = {
-              --"Class",
-              --"Function",
-              --"Method",
-              --"Constructor",
-              --"Interface",
-              --"Module",
-              --"Struct",
-              --"Trait",
-              --"Field",
-              --"Property",
-            --},
-          --}),
-          --desc = "Goto Symbol",
-        --},
-        --{
-          --"<leader>sS",
-          --Util.telescope("lsp_dynamic_workspace_symbols", {
-            --symbols = {
-              --"Class",
-              --"Function",
-              --"Method",
-              --"Constructor",
-              --"Interface",
-              --"Module",
-              --"Struct",
-              --"Trait",
-              --"Field",
-              --"Property",
-            --},
-          --}),
-          --desc = "Goto Symbol (Workspace)",
-        --},
+        { "<leader>ss","<cmd>Telescope lsp_references<cr>",desc = "Goto Symbol"},
       },
       opts = {
         defaults = {
@@ -188,7 +145,7 @@ return {
           end,
           desc = "Explorer NeoTree (cwd)",
         },
-        { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+        { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
       },
       deactivate = function()
         vim.cmd([[Neotree close]])
