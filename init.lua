e6a85438a15f7690d40cd64d38dbe3fdefb39f6a
aadriance <primaryartemis@outlook.com>
2023-06-06 22:58:40 -0700

    fix-up autoclose of neotree

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-local this_is_a_test = ""
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -1,13 +1,30 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    event_handlers = {
-      {
-        event = "file_opened",
-        handler = function()
-          --auto close
-          require("neo-tree").close_all()
-        end,
+  {
+    "folke/edgy.nvim",
+    opts = {
+      left = {
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            --auto close
+            require("neo-tree").close_all()
+          end,
+        },
       },
     },
   },
