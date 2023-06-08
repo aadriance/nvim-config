59776bcfbb634e13ca4f153d4e88cee2166195a1
aadriance <primaryartemis@outlook.com>
2023-06-08 12:02:24 -0700

    autocmd for C options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---80 cols of code is enough for anyone!
-vim.opt.colorcolumn = "80"
 
 --Keep cursor centerish, but don't move for small adjustments
 vim.o.scrolloff = 10
@@ -11,9 +9,15 @@ vim.o.scrolloff = 10
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
---Tabs are not welcome here
-if vim.bo.filetype == "c" then
+function Set_C_Opts()
   vim.o.expandtab = true
   vim.o.tabstop = 4
   vim.o.shiftwidth = 4
+  --80 cols of code is enough for anyone!
+  vim.opt.colorcolumn = "80"
 end
+
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  { pattern = { "*.c", "*.h", "*.w", "*.cpp", "*.hpp" }, callback = Set_C_Opts }
+)
